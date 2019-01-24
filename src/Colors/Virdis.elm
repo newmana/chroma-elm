8b5bf5b9175457026dc8c392a45b81583181fc98
newmana <andrewfnewman@gmail.com>
2019-01-24 11:18:10 +1000

    Add virdis.

---
+++ src/Colors/Virdis.elm
@@ -0,0 +1,33 @@
+module Colors.Virdis exposing (virdis)
+
+import Color exposing (Color, rgb255)
+import List.Nonempty as Nonempty
+
+
+
+-- Sequential
+
+
+orRd : Nonempty.Nonempty Color
+orRd =
+    Nonempty.Nonempty (rgb255 68 1 84)
+        [ rgb255 72 21 103
+        , rgb255 72 38 119
+        , rgb255 69 55 129
+        , rgb255 64 71 136
+        , rgb255 57 86 140
+        , rgb255 51 99 141
+        , rgb255 45 112 142
+        , rgb255 40 125 142
+        , rgb255 35 138 141
+        , rgb255 31 150 139
+        , rgb255 32 163 135
+        , rgb255 41 175 127
+        , rgb255 60 187 117
+        , rgb255 85 193 103
+        , rgb255 115 208 85
+        , rgb255 149 216 64
+        , rgb255 184 222 41
+        , rgb255 220 227 25
+        , rgb255 253 231 37
+        ]
