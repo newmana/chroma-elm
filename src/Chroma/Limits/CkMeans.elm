3b331fc3068665fbd9c0846b96c14dfd11fe3bad
Andrew Newman <andrewfnewman@gmail.com>
2019-07-17 19:09:47 +1000

    Fix.

--- src/Chroma/Limits/CkMeans.elm
+++ src/Chroma/Limits/CkMeans.elm
@@ -1,7 +1,6 @@
 module Chroma.Limits.CkMeans exposing (limit)
 
 import Chroma.Limits.Analyze as Analyze
-import List.Extra as ListExtra
 import List.Nonempty as Nonempty
 
 
@@ -9,7 +8,7 @@ limit : Int -> Analyze.Scale -> Nonempty.Nonempty Float
 limit bins scale =
     let
         numberUnique =
-            ListExtra.unique scale.values |> List.length
+            Nonempty.uniq scale.values |> Nonempty.length
     in
     if numberUnique == 1 then
         scale.values
@@ -39,9 +38,30 @@ fillMatrices scale =
                 Nonempty.get (el - 1) scale.values + shiftedValue
 
         sums =
-            Nonempty.map (\el -> init el) (Nonempty.Nonempty 0 (List.range 1 (nValues - 1))
+            Nonempty.map (\el -> init el) (Nonempty.Nonempty 0 (List.range 1 (nValues - 1)))
 
         sumsOfSquares =
             Nonempty.map (\el -> el * el) sums
     in
     Debug.todo ""
+
+
+ssq : Int -> Int -> Nonempty.Nonempty Float -> Nonempty.Nonempty Float -> Float
+ssq j i sums sumsOfSquares =
+    let
+        sji =
+            if j > 0 then
+                let
+                    muji =
+                        Nonempty.get i sums + Nonempty.get (j - 1) sums / (i - j + 1 |> toFloat)
+                in
+                Nonempty.get i sumsOfSquares - Nonempty.get (j - 1) sumsOfSquares - (i - j + 1 |> toFloat) * muji * muji
+
+            else
+                Nonempty.get i sumsOfSquares - (Nonempty.get i sums * Nonempty.get i sums) / (i + 1 |> toFloat)
+    in
+    if sji < 0 then
+        0
+
+    else
+        sji
