23c02c21aa08d019e0ec0c7899eeb92a44964553
Andrew Newman <andrewfnewman@gmail.com>
2019-07-22 17:20:08 +1000

    Fix add all tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,8 +4,13 @@ import Chroma.ChromaTest
 import Chroma.Converter.Cmyk2RgbTest
 import Chroma.Converter.Hex2RgbTest
 import Chroma.Converter.Lab2RgbTest
+import Chroma.Converter.Lch2LabTest
 import Chroma.Converter.ToHexTest
 import Chroma.InterpolatorTest
+import Chroma.Ops.AlphaTest
+import Chroma.Ops.LightnessTest
+import Chroma.Ops.LuminanceTest
+import Chroma.Ops.SaturateTest
 import Test as Test
 
 
@@ -17,5 +22,10 @@ all =
         , Chroma.Converter.Cmyk2RgbTest.tests
         , Chroma.Converter.Hex2RgbTest.tests
         , Chroma.Converter.Lab2RgbTest.tests
+        , Chroma.Converter.Lch2LabTest.tests
         , Chroma.Converter.ToHexTest.tests
+        , Chroma.Ops.AlphaTest.tests
+        , Chroma.Ops.LightnessTest.tests
+        , Chroma.Ops.LuminanceTest.tests
+        , Chroma.Ops.SaturateTest.tests
         ]
