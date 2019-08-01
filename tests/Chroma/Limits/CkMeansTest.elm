7af2880e06c475bb4e6be927fbbc68265b2d57c7
Andrew Newman <andrewfnewman@gmail.com>
2019-08-01 18:46:12 +1000

    Remove dupe.

--- tests/Chroma/Limits/CkMeansTest.elm
+++ tests/Chroma/Limits/CkMeansTest.elm
@@ -17,27 +17,37 @@ tests =
         ]
 
 
+simple : Nonempty.Nonempty Float
+simple =
+    Nonempty.Nonempty 1 [ 3, 3, 3, 4 ]
+
+
+simpleStats : Nonempty.Nonempty Float
+simpleStats =
+    Nonempty.Nonempty -1 [ 2, -1, 2, 4, 5, 6, -1, 2, -1 ]
+
+
 testLimit : Test.Test
 testLimit =
     Test.describe "Get ckmeans limit breaks"
         [ Test.test "Simple 3 breaks" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty 1 [ 3, 3, 3, 4 ])
+                Analyze.analyze simple
                     |> CkMeans.limit 3
                     |> Expect.equal (Nonempty.Nonempty 1 [ 3, 4 ])
         , Test.test "Simple 4 breaks" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty 1 [ 3, 3, 3, 4 ])
+                Analyze.analyze simple
                     |> CkMeans.limit 4
                     |> Expect.equal (Nonempty.Nonempty 1 [ 3, 4 ])
         , Test.test "3 breaks from Simple Stats" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty -1 [ 2, -1, 2, 4, 5, 6, -1, 2, -1 ])
+                Analyze.analyze simpleStats
                     |> CkMeans.limit 3
                     |> Expect.equal (Nonempty.Nonempty -1 [ 2, 4 ])
         , Test.test "4 breaks from Simple Stats" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty -1 [ 2, -1, 2, 4, 5, 6, -1, 2, -1 ])
+                Analyze.analyze simpleStats
                     |> CkMeans.limit 4
                     |> Expect.equal (Nonempty.Nonempty -1 [ 2, 4, 6 ])
         ]
@@ -48,22 +58,22 @@ testBinned =
     Test.describe "Get ckmeans binned breaks"
         [ Test.test "Simple 3 breaks" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty 1 [ 3, 3, 3, 4 ])
+                Analyze.analyze simple
                     |> CkMeans.binned 3
                     |> Expect.equal (Nonempty.Nonempty (Array.fromList [ 1 ]) [ Array.fromList [ 3, 3, 3 ], Array.fromList [ 4 ] ])
         , Test.test "Simple 4 breaks" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty 1 [ 3, 3, 3, 4 ])
+                Analyze.analyze simple
                     |> CkMeans.binned 4
                     |> Expect.equal (Nonempty.Nonempty (Array.fromList [ 1 ]) [ Array.fromList [ 3, 3 ], Array.fromList [ 3 ], Array.fromList [ 4 ] ])
         , Test.test "3 breaks from Simple Stats" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty -1 [ 2, -1, 2, 4, 5, 6, -1, 2, -1 ])
+                Analyze.analyze simpleStats
                     |> CkMeans.binned 3
                     |> Expect.equal (Nonempty.Nonempty (Array.fromList [ -1, -1, -1, -1 ]) [ Array.fromList [ 2, 2, 2 ], Array.fromList [ 4, 5, 6 ] ])
         , Test.test "4 breaks from Simple Stats" <|
             \_ ->
-                Analyze.analyze (Nonempty.Nonempty -1 [ 2, -1, 2, 4, 5, 6, -1, 2, -1 ])
+                Analyze.analyze simpleStats
                     |> CkMeans.binned 4
                     |> Expect.equal (Nonempty.Nonempty (Array.fromList [ -1, -1, -1, -1 ]) [ Array.fromList [ 2, 2, 2 ], Array.fromList [ 4, 5 ], Array.fromList [ 6 ] ])
         ]
