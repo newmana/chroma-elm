2cbf80feb27ba7e3d43aaa092011e69f27aaea3e
Andrew Newman <andrewfnewman@gmail.com>
2019-09-05 08:56:20 +1000

    Luminance parameters around the wrong way.

--- examples/Page/ColorOperations.elm
+++ examples/Page/ColorOperations.elm
@@ -3,8 +3,10 @@ module Page.ColorOperations exposing (content)
 import Chroma.Chroma as Chroma
 import Chroma.Colors.W3CX11 as W3CX11
 import Chroma.Converter.Out.ToHex as ToHex
+import Chroma.Converter.Out.ToLab as ToLab
 import Chroma.Ops.Alpha as OpsAlpha
 import Chroma.Ops.Lightness as OpsLightness
+import Chroma.Ops.Luminance as OpsLuminance
 import Chroma.Ops.Saturate as OpsSaturate
 import Chroma.Types as Types
 import Color as Color
@@ -24,196 +26,284 @@ content =
          , Html.p
             [ HtmlAttributes.class "subtitle" ]
             [ Html.text "Functions to modify an existing color value" ]
+         , Html.p
+            [ HtmlAttributes.class "subtitle" ]
+            [ Html.text "Alpha" ]
          ]
-            ++ example1
-            ++ example2
-            ++ example3
-            ++ example4
-            ++ example5
-            ++ example6
-            ++ example7
-            ++ example8
+            ++ setAlpha
+            ++ getAlpha
+            ++ Page.p "\u{00A0}"
+            ++ [ Html.p
+                    [ HtmlAttributes.class "subtitle" ]
+                    [ Html.text "Lightness" ]
+               ]
+            ++ darkenOne
+            ++ darkenTwo
+            ++ brightenTwo
+            ++ brightenThree
+            ++ Page.p "\u{00A0}"
+            ++ [ Html.p
+                    [ HtmlAttributes.class "subtitle" ]
+                    [ Html.text "Luminance" ]
+               ]
+            ++ getLuminance
+            ++ setLuminanceRgb
+            ++ setLuminanceLab
+            ++ Page.p "\u{00A0}"
+            ++ [ Html.p
+                    [ HtmlAttributes.class "subtitle" ]
+                    [ Html.text "Saturate" ]
+               ]
+            ++ saturateTwo
+            ++ desaturateThree
         )
     ]
 
 
-example1 : List (Html.Html msg)
-example1 =
-    Page.example "has-text-white" example1Code example1SourceCode example1Output
+setAlpha : List (Html.Html msg)
+setAlpha =
+    Page.example "has-text-white" setAlphaCode setAlphaSourceCode setAlphaOutput
 
 
-example1Code : String
-example1Code =
+setAlphaCode : String
+setAlphaCode =
     Chroma.chroma "red" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsAlpha.setAlpha 0.5 |> ToHex.toHexAlpha
 
 
-example1SourceCode : String
-example1SourceCode =
+setAlphaSourceCode : String
+setAlphaSourceCode =
     """Chroma.chroma "red"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsLightness.alpha 0.5
  |> ToHex.toHexAlpha  """
 
 
-example1Output : List (Html.Html msg)
-example1Output =
-    [ Html.text ("\"" ++ example1Code ++ "\" : String ") ]
+setAlphaOutput : List (Html.Html msg)
+setAlphaOutput =
+    [ Html.text ("\"" ++ setAlphaCode ++ "\" : String ") ]
 
 
-example2 : List (Html.Html msg)
-example2 =
-    Page.example "has-text-black" "" example2SourceCode example2Output
+getAlpha : List (Html.Html msg)
+getAlpha =
+    Page.example "has-text-black" "" getAlphaSourceCode getAlphaOutput
 
 
-example2Code : String
-example2Code =
+getAlphaCode : String
+getAlphaCode =
     Types.RGBAColor (Color.rgba 1 0 0 0.35) |> OpsAlpha.alpha |> String.fromFloat
 
 
-example2SourceCode : String
-example2SourceCode =
+getAlphaSourceCode : String
+getAlphaSourceCode =
     """Types.RGBAColor (Color.rgba 1 0 0 0.35)
  |> OpsAlpha.alpha """
 
 
-example2Output : List (Html.Html msg)
-example2Output =
-    [ Html.text ("\"" ++ example2Code ++ "\" : Float ") ]
+getAlphaOutput : List (Html.Html msg)
+getAlphaOutput =
+    [ Html.text ("\"" ++ getAlphaCode ++ "\" : Float ") ]
 
 
-example3 : List (Html.Html msg)
-example3 =
-    Page.example "has-text-white" example3Code example3SourceCode example3Output
+darkenOne : List (Html.Html msg)
+darkenOne =
+    Page.example "has-text-white" darkenOneCode darkenOneSourceCode darkenOneOutput
 
 
-example3Code : String
-example3Code =
+darkenOneCode : String
+darkenOneCode =
     Chroma.chroma "hotpink" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsLightness.darken 1.0 |> ToHex.toHex
 
 
-example3SourceCode : String
-example3SourceCode =
+darkenOneSourceCode : String
+darkenOneSourceCode =
     """Chroma.chroma "hotpink"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsLightness.darken 1.0
  |> ToHex.toHex """
 
 
-example3Output : List (Html.Html msg)
-example3Output =
+darkenOneOutput : List (Html.Html msg)
+darkenOneOutput =
     [ Html.text """"#c93384" : String  """ ]
 
 
-example4 : List (Html.Html msg)
-example4 =
-    Page.example "has-text-white" example4Code example4SourceCode example4Output
+darkenTwo : List (Html.Html msg)
+darkenTwo =
+    Page.example "has-text-white" darkenTwoCode darkenTwoSourceCode darkenTwoOutput
 
 
-example4Code : String
-example4Code =
+darkenTwoCode : String
+darkenTwoCode =
     Chroma.chroma "hotpink" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsLightness.darken 2.0 |> ToHex.toHex
 
 
-example4SourceCode : String
-example4SourceCode =
+darkenTwoSourceCode : String
+darkenTwoSourceCode =
     """Chroma.chroma "hotpink"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsLightness.darken 2.0
  |> ToHex.toHex """
 
 
-example4Output : List (Html.Html msg)
-example4Output =
+darkenTwoOutput : List (Html.Html msg)
+darkenTwoOutput =
     [ Html.text """"#c930058" : String  """ ]
 
 
-example5 : List (Html.Html msg)
-example5 =
-    Page.example "has-text-black" example5Code example5SourceCode example5Output
+brightenTwo : List (Html.Html msg)
+brightenTwo =
+    Page.example "has-text-black" brightenTwoCode brightenTwoSourceCode brightenTwoOutput
 
 
-example5Code : String
-example5Code =
+brightenTwoCode : String
+brightenTwoCode =
     Chroma.chroma "hotpink" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsLightness.brighten 2.0 |> ToHex.toHex
 
 
-example5SourceCode : String
-example5SourceCode =
+brightenTwoSourceCode : String
+brightenTwoSourceCode =
     """Chroma.chroma "hotpink"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsLightness.brighten 2.0
  |> ToHex.toHex """
 
 
-example5Output : List (Html.Html msg)
-example5Output =
+brightenTwoOutput : List (Html.Html msg)
+brightenTwoOutput =
     [ Html.text """"#ffd1ff" : String  """ ]
 
 
-example6 : List (Html.Html msg)
-example6 =
-    Page.example "has-text-black" example6Code example6SourceCode example6Output
+brightenThree : List (Html.Html msg)
+brightenThree =
+    Page.example "has-text-black" brightenThreeCode brightenThreeSourceCode brightenThreeOutput
 
 
-example6Code : String
-example6Code =
+brightenThreeCode : String
+brightenThreeCode =
     Chroma.chroma "hotpink" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsLightness.brighten 3.0 |> ToHex.toHex
 
 
-example6SourceCode : String
-example6SourceCode =
+brightenThreeSourceCode : String
+brightenThreeSourceCode =
     """Chroma.chroma "hotpink"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsLightness.brighten 3.0
  |> ToHex.toHex """
 
 
-example6Output : List (Html.Html msg)
-example6Output =
+brightenThreeOutput : List (Html.Html msg)
+brightenThreeOutput =
     [ Html.text """"#ffffff" : String  """ ]
 
 
-example7 : List (Html.Html msg)
-example7 =
-    Page.example "has-text-white" example7Code example7SourceCode example7Output
+getLuminance : List (Html.Html msg)
+getLuminance =
+    Page.example "has-text-black" getLuminanceCode getLuminanceSourceCode getLuminanceOutput
 
 
-example7Code : String
-example7Code =
+getLuminanceCode : String
+getLuminanceCode =
+    Types.RGBAColor W3CX11.aquamarine |> OpsLuminance.luminance |> String.fromFloat
+
+
+getLuminanceSourceCode : String
+getLuminanceSourceCode =
+    """Types.RGBAColor W3CX11.aquamarine
+ |> OpsLuminance.luminance """
+
+
+getLuminanceOutput : List (Html.Html msg)
+getLuminanceOutput =
+    [ Html.text (getLuminanceCode ++ " : Float ") ]
+
+
+setLuminanceRgb : List (Html.Html msg)
+setLuminanceRgb =
+    Page.example "has-text-black" setLuminanceRgbCode setLuminanceRgbSourceCode setLuminanceRgbOutput
+
+
+setLuminanceRgbCode : String
+setLuminanceRgbCode =
+    Types.RGBAColor W3CX11.aquamarine |> (\x -> OpsLuminance.setLuminance x 0.5) |> ToHex.toHex
+
+
+setLuminanceRgbSourceCode : String
+setLuminanceRgbSourceCode =
+    """Types.RGBAColor W3CX11.aquamarine
+ |> (\\x -> OpsLuminance.setLuminance x 0.5)
+ |> ToHex.toHex """
+
+
+setLuminanceRgbOutput : List (Html.Html msg)
+setLuminanceRgbOutput =
+    [ Html.text """"#67ceab" : String  """ ]
+
+
+setLuminanceLab : List (Html.Html msg)
+setLuminanceLab =
+    Page.example "has-text-black" setLuminanceLabCode setLuminanceLabSourceCode setLuminanceLabOutput
+
+
+setLuminanceLabCode : String
+setLuminanceLabCode =
+    Types.RGBAColor W3CX11.aquamarine |> ToLab.toLab |> Types.LABColor |> (\x -> OpsLuminance.setLuminance x 0.5) |> ToHex.toHex
+
+
+setLuminanceLabSourceCode : String
+setLuminanceLabSourceCode =
+    """Types.RGBAColor W3CX11.aquamarine
+ |> ToLab.toLab
+ |> Types.LABColor
+ |> (\\x -> OpsLuminance.setLuminance x 0.5)
+ |> ToHex.toHex """
+
+
+setLuminanceLabOutput : List (Html.Html msg)
+setLuminanceLabOutput =
+    [ Html.text """"#67ceab" : String  """ ]
+
+
+saturateTwo : List (Html.Html msg)
+saturateTwo =
+    Page.example "has-text-white" saturateTwoCode saturateTwoSourceCode saturateTwoOutput
+
+
+saturateTwoCode : String
+saturateTwoCode =
     Chroma.chroma "slategray" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsSaturate.saturate 2.0 |> ToHex.toHex
 
 
-example7SourceCode : String
-example7SourceCode =
+saturateTwoSourceCode : String
+saturateTwoSourceCode =
     """Chroma.chroma "slategray"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsSaturate.saturate 2.0
  |> ToHex.toHex """
 
 
-example7Output : List (Html.Html msg)
-example7Output =
+saturateTwoOutput : List (Html.Html msg)
+saturateTwoOutput =
     [ Html.text """"#0087cd" : String  """ ]
 
 
-example8 : List (Html.Html msg)
-example8 =
-    Page.example "has-text-white" example8Code example8SourceCode example8Output
+desaturateThree : List (Html.Html msg)
+desaturateThree =
+    Page.example "has-text-white" desaturateThreeCode desaturateThreeSourceCode desaturateThreeOutput
 
 
-example8Code : String
-example8Code =
+desaturateThreeCode : String
+desaturateThreeCode =
     Chroma.chroma "hotpink" |> Result.withDefault (Types.RGBAColor W3CX11.black) |> OpsSaturate.desaturate 3.0 |> ToHex.toHex
 
 
-example8SourceCode : String
-example8SourceCode =
+desaturateThreeSourceCode : String
+desaturateThreeSourceCode =
     """Chroma.chroma "hotpink"
  |> Result.withDefault (Types.RGBAColor W3CX11.black)
  |> OpsSaturate.desaturate 3.0
  |> ToHex.toHex """
 
 
-example8Output : List (Html.Html msg)
-example8Output =
+desaturateThreeOutput : List (Html.Html msg)
+desaturateThreeOutput =
     [ Html.text """"#b199a3" : String  """ ]
